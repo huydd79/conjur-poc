86c67031db2ccd28ca6ec4dc9b0abdcb5187cc5e
Huy Do <86530496+huydd79@users.noreply.github.com>
2024-04-13 19:23:25 +0700

    Update 02.conjur-restore.sh
    Modify to match folder name

--- test-backup-restore/02.conjur-restore.sh
+++ test-backup-restore/02.conjur-restore.sh
@@ -6,6 +6,7 @@ set +x
 
 data_file="$(ls -t /opt/cyberark/conjur/backups/*.gpg | head -n1)"
 key_file="/opt/cyberark/conjur/backups/key"
+data_file_name="$(basename $data_file)"
 
 if [ ! -f $data_file ] || [ ! -f $key_file ]; then
     echo "Backup data is not existed. Please run backup command before doing restore!!!"
@@ -14,9 +15,6 @@ else
     echo "Backup data found. Starting restore process... "
     echo "Key file: $key_file"
     echo "Data file: $data_file"
-    docker exec $node_name evoke unpack backup --key $key_file $data_file
+    docker exec $node_name evoke unpack backup --key /opt/conjur/backup/key /opt/conjur/backup/$data_file_name
     docker exec $node_name evoke restore --accept-eula
 fi
-
-
-set -x
