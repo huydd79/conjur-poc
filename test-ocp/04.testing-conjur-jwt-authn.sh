521621f389baffe01200cfa6b906292462a713c6
Huy Do <86530496+huydd79@users.noreply.github.com>
2025-02-28 13:46:21 +0700

    Update 04.testing-conjur-jwt-authn.sh

--- test-ocp/04.testing-conjur-jwt-authn.sh
+++ test-ocp/04.testing-conjur-jwt-authn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -x
-JWT=$(cat gitlab.jwt)
+JWT=$(cat test.jwt)
 
-SESSIONTOKEN=$(curl -k -X POST https://conjur.home.huydo.net:8443/authn-jwt/gitlab/DEMO/authenticate -H "Content-Type: application/x-www-form-urlencoded" -H "Accept-Encoding: base64" --data-urlencode "jwt=$JWT")
+SESSIONTOKEN=$(curl -k -X POST https://conjur.home.huydo.net:8443/authn-jwt/ocp/DEMO/authenticate -H "Content-Type: application/x-www-form-urlencoded" -H "Accept-Encoding: base64" --data-urlencode "jwt=$JWT")
 echo "======== TEST RESULT ======="
 echo $SESSIONTOKEN
 set +x
