85837023427efd49869f21212ea2dc2a85f15ec4
Huy Do <86530496+huydd79@users.noreply.github.com>
2025-03-26 20:40:47 +0700

    Update 00.config.sh
    Update for latest conjur version

--- 00.config.sh
+++ 00.config.sh
@@ -8,7 +8,7 @@ SUDO=
 CONTAINER_MGR=podman
 
 #IP addresses of conjur and crc VM
-CONJUR_IP=172.16.100.57
+CONJUR_IP=10.0.0.1
 POC_IP=$CONJUR_IP
 POC_DOMAIN=poc.local
 POC_CONJUR_ADMIN_PW=ChangeMe123!
@@ -16,7 +16,7 @@ POC_CONJUR_ACCOUNT=POC
 POC_CONJUR_HTTPS_PORT=443
 #Path to folder with all docker images
 UPLOAD_DIR=/opt/upload
-conjur_appliance_file=conjur-appliance-Rls-v13.2.0.tar.gz
-conjur_version=13.2.2
+conjur_appliance_file=conjur-appliance-Rls-v13.5.0.tar.gz
+conjur_version=13.5.0
 #Conjur container name
 node_name=conjur
