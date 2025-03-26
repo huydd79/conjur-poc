1a2cc8d40124f7a06854dd21cf38b2327486ab77
Huy Do <86530496+huydd79@users.noreply.github.com>
2025-03-27 00:46:10 +0700

    Update 03.loading-jenkins-jwt-data.sh

--- test-jenkins/03.loading-jenkins-jwt-data.sh
+++ test-jenkins/03.loading-jenkins-jwt-data.sh
@@ -4,6 +4,6 @@ set -x
 conjur variable set -i conjur/authn-jwt/jenkins/jwks-uri -v http://10.0.0.2:8080/jwtauth/conjur-jwk-set
 conjur variable set -i conjur/authn-jwt/jenkins/token-app-property -v jenkins_full_name
 conjur variable set -i conjur/authn-jwt/jenkins/identity-path -v jwt-apps/jenkins
-conjur variable set -i conjur/authn-jwt/jenkins/issuer -v http://jenkins.poc.local:8080
+conjur variable set -i conjur/authn-jwt/jenkins/issuer -v http://10.0.0.2:8080/
 
 set +x
