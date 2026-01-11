13e5bd348adf69f01f78190d30cd546fdc42929e
Huy Do <huydd@huydo.net>
2026-01-11 10:00:23 +0000

    20260111

--- 00.config.sh
+++ 00.config.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #Change your configuration and set READY=true when done
-READY=false
+READY=true
 
 #Container env setting
 SUDO=
-CONTAINER_MGR=podman
+CONTAINER_MGR=docker
 
 #IP addresses of conjur and crc VM
-CONJUR_IP=10.0.0.2
+CONJUR_IP=172.16.100.8
 POC_IP=$CONJUR_IP
-POC_DOMAIN=poc.local
+POC_DOMAIN=cybr.huydo.net
 POC_CONJUR_ADMIN_PW=ChangeMe123!
 POC_CONJUR_ACCOUNT=POC
-POC_CONJUR_HTTPS_PORT=443
+POC_CONJUR_HTTPS_PORT=8443
 #Path to folder with all docker images
 UPLOAD_DIR=/opt/upload
-conjur_appliance_file=conjur-appliance-Rls-v13.5.0.tar.gz
-conjur_version=13.5.0
+conjur_appliance_file=conjur-appliance-Rls-v13.7.0.tar.gz
+conjur_version=13.7.0
 #Conjur container name
 node_name=conjur
